1e08e95e272dea37c149167b1bf5ce451a507f79
Joshua-Bonner <joshua.bonner@outlook.com>
2020-03-08 13:00:07 -0400

    Created start of Wak'a Birb project

---
+++ app/src/main/java/com/hnb/wakabirb/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hnb.wakabirb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
