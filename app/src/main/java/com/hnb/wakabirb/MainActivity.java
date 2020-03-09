2608beed6885fb7ae63b3fc9ffb3f266998c03ab
Joshua-Bonner <joshua.bonner@outlook.com>
2020-03-08 20:30:55 -0400

    Title activity now transitions to Game activity

--- app/src/main/java/com/hnb/wakabirb/MainActivity.java
+++ app/src/main/java/com/hnb/wakabirb/MainActivity.java
@@ -4,14 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText userName = findViewById(R.id.EditUsrName);
+        final Button startGameBtn = findViewById(R.id.StartButton);
+
+        //Add textWatcher to notify the user
+        userName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().trim().length()==0) {
+                    startGameBtn.setEnabled(false);
+                    startGameBtn.setVisibility(View.INVISIBLE);
+                    Toast.makeText(MainActivity.this, "Text can not be empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    startGameBtn.setVisibility(View.VISIBLE);
+                    startGameBtn.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     public void StartGame(View button){
