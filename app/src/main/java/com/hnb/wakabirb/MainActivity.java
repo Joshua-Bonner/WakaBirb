0e0355854f40a8581ca62c39ad2fc2a3be5fbe05
Alyssa <a_b_abram_0510@outlook.com>
2020-03-28 19:11:23 -0400

    started sharedPreferences, added toolbar to mainActivty

--- app/src/main/java/com/hnb/wakabirb/MainActivity.java
+++ app/src/main/java/com/hnb/wakabirb/MainActivity.java
@@ -1,22 +1,37 @@
 package com.hnb.wakabirb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+
+    Boolean musicOn;
+    Boolean soundEffectsOn;
+    public static final String myPreference = "mypref";
+    public static final String mOnString = "musicOnKey";
+    public static final String seOn = "seOnKey";
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
 
         EditText userName = findViewById(R.id.EditUsrName);
         final Button startGameBtn = findViewById(R.id.StartButton);
@@ -46,6 +61,21 @@ public class MainActivity extends AppCompatActivity {
                 // TODO: QUERY FOR THE EXISTENCE OF THE USERNAME AND RETRIEVE ASSOCIATED DATA
             }
         });
+
+        sharedPreferences = getSharedPreferences(myPreference, Context.MODE_PRIVATE);
+        if (sharedPreferences.contains(mOnString)){
+            musicOn = sharedPreferences.getBoolean(mOnString, true);    //get value set by user, else set musicOn to true
+        }
+        if ( sharedPreferences.contains(seOn)){
+            soundEffectsOn = sharedPreferences.getBoolean(seOn, true);  //get value set by user, else set soundEffectsOn to true
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     public void onClickStartGame(View button){
---
+++ app/src/main/java/com/hnb/wakabirb/SettingsActivity.java
@@ -0,0 +1,24 @@
+package com.hnb.wakabirb;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    public static class SettingsFragment extends PreferenceFragment{
+        @Override
+        public void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+}
